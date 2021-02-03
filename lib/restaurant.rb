b24777c1b232428c9621a1c79532a5c337364f3c
joemecha <jwmecha@gmail.com>
2021-02-03 14:19:52 -0600

    Completed 'Iteration 2'

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -6,4 +6,13 @@ class Restaurant
     @name = name
     @dishes = []
   end
+
+  def closing_time(duration_hours)
+    closing_time = opening_time[0, 2].to_i + duration_hours # Note: works for 2-digit hours
+    closing_time = closing_time.to_s + ':00'
+  end
+
+  def add_dish(dish)
+    @dishes << dish
+  end
 end
