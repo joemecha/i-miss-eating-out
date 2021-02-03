da4dd31bc1f2a073f13b966ea04edba17d139cfa
joemecha <jwmecha@gmail.com>
2021-02-03 14:25:23 -0600

    Refractored so #closing_time can handle single digit hours

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -8,8 +8,10 @@ class Restaurant
   end
 
   def closing_time(duration_hours)
-    closing_time = opening_time[0, 2].to_i + duration_hours # Note: works for 2-digit hours
-    closing_time = closing_time.to_s + ':00'
+    # closing_time = opening_time[0, 2].to_i + duration_hours # Note: works for 2-digit hours
+    # closing_time.to_s + ':00'
+    opening_hour = opening_time.split(":").first.to_i
+    closing_time = (opening_hour + duration_hours).to_s + ':00'
   end
 
   def add_dish(dish)
